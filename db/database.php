8304095eb6c647afa46ece67bb9f749719c05eaa
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-16 16:22:16 +0200

    Esercizio terminato

---
+++ db/database.php
@@ -0,0 +1,56 @@
+<?php 
+
+$dischiArray = [
+    [
+        'titolo' => 'Santeria',
+        'artista' => 'Gue Pequeno',
+        'copertina' => 'http://www.ansa.it/webimages/img_700/2016/6/6/8defbe8b5d7968b378d1f31c9507043f.jpg',
+    ],
+    [
+        'titolo' => 'Famoso',
+        'artista' => 'Sfera Ebbasta',
+        'copertina' => 'https://images-na.ssl-images-amazon.com/images/I/91Bjp2F0aGL._AC_SL1500_.jpg',
+    ],
+    [
+        'titolo' => 'Blu Celeste',
+        'artista' => 'Blanco',
+        'copertina' => 'https://a6p8a2b3.stackpathcdn.com/zE_EzJ3_0y-vh4lHptsKkeXXV8w=/1200x1200/smart/rockol-img/img/foto/upload/bluceleste-cover.jpg',
+    ],
+    [
+        'titolo' => 'Untouchable',
+        'artista' => 'Tony Effe',
+        'copertina' => 'https://images-na.ssl-images-amazon.com/images/I/A1dIEmkQLWS._AC_SL1500_.jpg',
+    ],
+    [
+        'titolo' => 'Keta Music vol.3',
+        'artista' => 'Emis Killa',
+        'copertina' => 'https://www.newsic.it/wp-content/uploads/2021/07/emis-kill-keta-music-3-album-2021.jpg',
+    ],
+    [
+        'titolo' => 'Supereroe',
+        'artista' => 'Emis Killa',
+        'copertina' => 'https://a6p8a2b3.stackpathcdn.com/cmPtV3H55aKTnNandWzKTWtGiyY=/1200x1200/smart/rockol-img/img/foto/upload/supereroe-emis-killa-cover-ts1539308393.jpeg',
+    ],
+    [
+        'titolo' => 'Gemelli',
+        'artista' => 'Ernia',
+        'copertina' => 'https://i1.wp.com/www.webl0g.net/wp-content/uploads/ERNIA-copertina-album-Gemelli-.jpg?fit=3000%2C3000&ssl=1',
+    ],
+    [
+        'titolo' => 'Ascendente',
+        'artista' => 'Ernia',
+        'copertina' => 'https://images-na.ssl-images-amazon.com/images/I/610v764PuiS._AC_SL1500_.jpg',
+    ],
+    [
+        'titolo' => 'Fast Life',
+        'artista' => 'GuePequeno',
+        'copertina' => 'https://a6p8a2b3.stackpathcdn.com/Y1kSzXtfSBHn5PcztFCJb-eVkv0=/1200x1200/smart/rockol-img/img/foto/upload/fastlife4-album-2021-600x600-1.jpg',
+    ],
+    [
+        'titolo' => 'Davide',
+        'artista' => 'Gemitaiz',
+        'copertina' => 'https://a6p8a2b3.stackpathcdn.com/iGIxUCh5YC8j9eT5hpOodM5ntN4=/300x300/smart/rockol-img/img/foto/upload/davide-gemitaiz-cover-ts1524185227.jpeg',
+    ],
+];
+
+?>
--- index.php
+++ index.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <?php 
+    require __DIR__ . "/db/database.php";
+    $imageUrl = $dischi['copertina'];
+    ?>
+
+    <div id="container-elements">
+        <?php foreach($dischiArray as $dischi) {?>
+            
+            <div class="container-disco">
+                <img src="<?php echo $dischi['copertina'];?>">
+                <h2><?php echo $dischi['titolo']?></h2>
+                <p><?php echo $dischi['artista']?></p>
+            </div>
+            
+        <?php } ?>
+    </div>
+
 </body>
 </html>
